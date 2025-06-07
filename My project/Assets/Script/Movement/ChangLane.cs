49f6ce8ef7fb5b84b6f6d0abfe3d8215ba30f8e1
SavE <nongsafgml@gmail.com>
2025-06-07 17:09:34 +0700

    movement
    movement 👍
    animation 👍

---
+++ My project/Assets/Script/Movement/ChangLane.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ChangLane : MonoBehaviour
+{
+    CharacterController Controller;
+    Animator Anim;
+
+    // lane varible
+    [SerializeField] int m_currentLane = 0; // left = -1 , center = 0 , right = 1
+    [SerializeField] int m_laneDistance = 1;
+
+    void Awake()
+    {
+        Controller = GetComponent<CharacterController>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"input : {Input.GetAxis("Horizontal")}");
+        Debug.Log($"m_currentLane : {m_currentLane}");
+        if (Input.GetKeyDown(KeyCode.D) && m_currentLane < 1)
+        {
+            m_currentLane++;
+            Debug.Log($"++");
+        }
+        if (Input.GetKeyDown(KeyCode.A) && m_currentLane > -1)
+        {
+            m_currentLane--;
+            Debug.Log($"--");
+        }
+
+
+        Vector3 targetPos = new Vector3(m_currentLane * m_laneDistance, transform.position.y, transform.position.z);
+
+        // Vector3 moveTo = Vector3.forward * m_Speed;
+
+        Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos, 10f * Time.deltaTime);
+
+        Vector3 moveDir = new Vector3(newPos.x - transform.position.x, 0, 0);
+
+        Controller.Move(moveDir);
+
+    }
+}
---
+++ My project/Assets/Script/Movement/MoveForward.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    CharacterController Controller;
+    [SerializeField] float m_Speed = 5f;
+    Animator Anim;
+
+    void Awake()
+    {
+        Controller = GetComponent<CharacterController>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = new Vector3(0, 0, 1);
+
+        Controller.Move(forward * m_Speed * Time.deltaTime);
+        if (transform.position.z > 0)
+        {
+            Anim.SetBool("IsRun?", true);
+        }
+        else
+        {
+            Anim.SetBool("IsRun?", false);
+        }
+        
+    }
+}
