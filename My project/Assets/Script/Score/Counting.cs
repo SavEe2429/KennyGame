77e3f7b1a124f038bdc00f323e13e817f9c8197d
SavE <nongsafgml@gmail.com>
2025-06-10 21:33:55 +0700

    score and score text
    เพิ่มคะแนนวิ่งแล้วก็ให้คะแนนแสดงแบบ  real time

---
+++ My project/Assets/Script/Score/Counting.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+
+public class Counting : MonoBehaviour
+{
+    enum Axis { X, Y, Z }
+    [SerializeField] Axis axis = Axis.Z;
+     [SerializeField] int decimalScore = 3;
+    [SerializeField] TextMeshProUGUI scoreLabel;
+    float startSpot;
+    float destination;
+
+    void Start()
+    {
+        startSpot = GetPosValue(transform.position);
+    }
+
+    void Update()
+    {
+        destination = GetPosValue(transform.position);
+        float score = destination - startSpot;
+
+        string format = $"F{decimalScore}";
+        scoreLabel.text = $"Score : {score.ToString(format)}";
+        // Debug.Log($"Score : {score.ToString(format)}");
+    }
+
+    float GetPosValue(Vector3 pos)
+    {
+        switch (axis)
+        {
+            case Axis.X: return pos.x;
+            case Axis.Y: return pos.y;
+            case Axis.Z: return pos.z;
+            default : return 0;
+        }
+    }
+}
